c6a5acf13e06c9cd6feba7bd404e26ab1d44bbfd
Francisco Freitas <fj.freitas@campus.fct.unl.pt>
2022-12-06 05:42:29 +0000

    Ex 2 done

--- Ex2P2.R
+++ Ex2P2.R
@@ -9,22 +9,22 @@
     #MLE
     MLE = function(x) {n/sum(log(x))}
     
-    MLE.os = MLE(os)
+    MLE.v = MLE(os)
     
-    MLE.os
+    MLE.v
     
     #MME
     MME = function(x){mean(x)/(mean(x)-1)}
     
-    MME.os = MME(os)
+    MME.v = MME(os)
     
-    MME.os
+    MME.v
     
   #2.2
-    In = n/(MLE.os^2)
-    MLE.v = 1/In
+    In = n/(MLE.v^2)
+    MLE.var = 1/In
     
-    MLE.v
+    MLE.var
   #2.3
     #Likelihood function
     likh = function(theta){
@@ -50,7 +50,7 @@
       return(s)
     }
     
-   #Graphical Display
+    #Graphical Display
     
     library(ggplot2)
     base <-
@@ -62,6 +62,7 @@
     base + xlab(expression(theta)) + ylab("log-likelihood") + geom_function(fun = loglikh)
     
     base + xlab(expression(theta)) + ylab("score") + geom_function(fun = scr)
+    
     
     #If we use interval estimation (2,4):
     
@@ -86,3 +87,160 @@
     
     #The ML estimate of alpha by R function maxLik() was 2.814179.
     
+  #2.5
+    #Bissection algorithm to estimate alpha
+    
+    bisection <- function(a,b,eps){
+      # [a,b]: interval where s verifies Bolzano’s theorem
+      # eps : is the stopping rule
+      alpha.it = vector(); alpha.it[1] = (a+b)/2
+        k = 1; diff = 1
+        diffs= vector()
+        while(diff>eps){
+          #If multiplication is negative, reduce b
+          if(scr(alpha.it[k])*scr(a)<0){
+            b = alpha.it[k]
+            alpha.it[k+1] = (a+b)/2
+          }
+          #If multiplication is positive, raise a
+          else{if(scr(alpha.it[k])*scr(a)>0){
+            a = alpha.it[k]
+            alpha.it[k+1] = (a+b)/2
+            #If multiplication result is 0, we apply the stopping rule
+          }else{alpha.it[k+1]=alpha.it[k]}
+          }
+          #Update the iteration difference for the stopping rule
+          diff = abs(alpha.it[k+1]-alpha.it[k])
+          diffs[k]=diff
+          k = k+1
+        }
+        result = as.matrix(alpha.it)
+        colnames(result)<-"iterations"
+        rownames(result)<-1:length(alpha.it)
+        retList<-list("res"=result,"error"=diffs)
+        retList
+      }
+      #Bisection result for each iteration
+      l1=bisection(2,4,1e-06)
+      m1=t(l1$res)
+      m1
+      
+      #Error result for each iteration
+      l1$error
+      
+      #(Newton-Raphson aux function)
+      prime <- function(alpha){
+        out = numeric(length(alpha))
+        if(length(alpha)==1){out =- n/alpha^2}
+        if(length(alpha)!=1){
+          for(i in 1:length(alpha)){out[i] = - n/alpha[i]^2}
+        }
+        return(out)
+      }
+      
+    #Newton-Raphson algorithm to estimate alpha
+     NR <- function(x,alpha0,eps){
+       # x: observed sample
+       # alpha0: first value to iterate over the vector
+       # eps: the stopping rule
+       alpha.it = vector()
+       alpha.it[1] = alpha0
+       k = 1
+       diff = 1
+       diffs=vector()
+       #iterations
+       while(diff>eps){
+         alpha.it[k+1] = alpha.it[k]-scr(alpha.it[k])/prime(alpha.it[k])
+         diff = abs(alpha.it[k+1]-alpha.it[k])
+         diffs[k]=diff
+         k = k+1
+       }
+       result = as.matrix(alpha.it)
+       colnames(result)<-"iterations"
+       rownames(result)<-1:length(alpha.it)
+       result
+       retList<-list("res"=result,"error"=diffs)
+       retList
+     }
+     #NR result for each iteration
+     l2=NR(os,MLE.var,1e-06)
+     m2=t(l2$res)
+     m2
+      
+     #Error result for each iteration
+     l2$error
+      
+     
+     #Secant algorithm to estimate alpha
+     secant <- function(x, alpha0, alpha1, eps) {
+       # x: alpha0 samples: 
+       # alpha0: interval left limit
+       # alpha1: interval right limit
+       # eps: stopping rule value
+       alpha.it = vector()
+       alpha.it[1] = alpha0
+       32
+       alpha.it[2] = alpha1
+       k = 2
+       diff = 1
+       diffs = vector()
+       diffs[1] = diff
+       # iterations
+       while (diff > eps) {
+         alpha.it[k + 1] = alpha.it[k] - scr(alpha.it[k]) * 
+           ((alpha.it[k] - alpha.it[k-1])/(scr(alpha.it[k]) - scr(alpha.it[k - 1])))
+         diff = abs(alpha.it[k + 1] - alpha.it[k])
+         diffs[k] = diff
+         k = k + 1
+       }
+       result = as.matrix(alpha.it)
+       colnames(result) <- "iterations"
+       rownames(result) <- 1:length(alpha.it)
+       retList <- list(res = result, error = diffs)
+       retList
+     }
+     #Secant result for each iteration
+     l3 = secant(os, 2, 4, 1e-06)
+     m3 = t(l3$res)
+     m3
+     
+     #Error result for each iteration
+     l3$error
+      
+    
+  #2.6
+     #I(alpha)
+     Ialpha<-function(alpha){
+       n/(alpha^2)
+     }
+     #Fisher Scoring algorithm
+     fisherScoring <- function(x,alpha0,eps){
+       # x: observed sample
+       # alpha0: first value to iterate over the vector
+       # eps: the stopping rule
+       alpha.it = vector()
+       alpha.it[1] = alpha0
+       k = 1
+       diff = 1
+       diffs=vector()
+       diffs[1]=diff
+       # iterations
+       while(diff>eps){
+         alpha.it[k+1] = alpha.it[k]+(1/Ialpha(alpha.it[k]))*scr(alpha.it[k])
+         diff = abs(alpha.it[k+1]-alpha.it[k])
+         k = k+1
+         diffs[k]=diff
+       }
+       result = as.matrix(alpha.it)
+       colnames(result)<-"iterations"
+       rownames(result)<-1:length(alpha.it)
+       retList<-list("res"=result,"error"=diffs)
+       retList
+     }
+     #Fisher Scoring result for each iteration
+     l4=fisherScoring(os,MLE.var,0.000001)
+     m4=t(l4$res)
+     m4
+     
+     #Error result for each iteration
+     l4$error
